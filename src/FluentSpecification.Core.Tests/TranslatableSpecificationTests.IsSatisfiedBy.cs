5df8e0816dede178dc3e0b4fe38329e3a2325275
Michał Kowal <michalpkowal@gmail.com>
2022-03-27 14:23:00 +0200

    Add Translatable specification (#14)

---
+++ src/FluentSpecification.Core.Tests/TranslatableSpecificationTests.IsSatisfiedBy.cs
@@ -0,0 +1,118 @@
+﻿using FluentSpecification.Abstractions.Validation;
+using FluentSpecification.Core.Tests.Data;
+using FluentSpecification.Core.Tests.Mocks;
+using FluentSpecification.Tests.Sdk;
+using System.Collections.Generic;
+using Xunit;
+
+namespace FluentSpecification.Core.Tests
+{
+    public partial class TranslatableSpecificationTests
+    {
+        public class IsSatisfiedBy
+        {
+            [Fact]
+            public void FalseSpecification_ReturnFalse()
+            {
+                var specification = MockSpecification.False();
+                var sut = new TranslatableSpecification<object>(specification, "Message");
+
+                var result = sut.IsSatisfiedBy(new object());
+
+                Assert.False(result);
+            }
+
+            [Fact]
+            public void NullCandidate_NoException()
+            {
+                var specification = MockSpecification.True();
+                var sut = new TranslatableSpecification<object>(specification, "Message");
+
+                var exception = Record.Exception(() => sut.IsSatisfiedBy(null));
+
+                Assert.Null(exception);
+            }
+
+            [Fact]
+            public void TrueSpecification_ReturnTrue()
+            {
+                var specification = MockSpecification.True();
+                var sut = new TranslatableSpecification<object>(specification, "Message");
+
+                var result = sut.IsSatisfiedBy(new object());
+
+                Assert.True(result);
+            }
+
+            [Fact]
+            public void RelatedTypes_NoException()
+            {
+                var specification = MockSpecification<IEnumerable<char>>.True();
+
+                var exception = Record.Exception(() =>
+                {
+                    var sut = new TranslatableSpecification<ChildFakeType>(specification, "Message");
+                    sut.IsSatisfiedBy(new ChildFakeType());
+                });
+
+                Assert.Null(exception);
+            }
+        }
+
+        public class IsSatisfiedBySpecificationResult
+        {
+            [Theory]
+            [CorrectValidationData(typeof(TranslatableData))]
+            public void TrueSpecification_ReturnExpectedResultObject(string message, SpecificationResult expected)
+            {
+                var specification = MockValidationSpecification.True();
+                var sut = new TranslatableSpecification<object>(specification, message);
+
+                var overall = sut.IsSatisfiedBy(new object(), out var result);
+
+                Assert.True(overall);
+                Assert.Equal(expected, result, new SpecificationResultComparer());
+            }
+
+            [Theory]
+            [IncorrectValidationData(typeof(TranslatableData))]
+            public void FalseSpecification_ReturnExpectedResultObject(string message, SpecificationResult expected)
+            {
+                var dum = new object();
+                var specification = MockValidationSpecification.False();
+                var sut = new TranslatableSpecification<object>(specification, message);
+
+                var overall = sut.IsSatisfiedBy(dum, out var result);
+
+                Assert.False(overall);
+                Assert.Equal(expected, result, new SpecificationResultComparer(dum));
+            }
+
+            [Theory]
+            [CorrectData(typeof(TranslatableData))]
+            public void NullCandidate_NoException(string message)
+            {
+                var specification = MockValidationSpecification.True();
+                var sut = new TranslatableSpecification<object>(specification, message);
+
+                var exception = Record.Exception(() => sut.IsSatisfiedBy(null, out _));
+
+                Assert.Null(exception);
+            }
+
+            [Fact]
+            public void RelatedTypes_NoException()
+            {
+                var specification = MockValidationSpecification<IEnumerable<char>>.True();
+
+                var exception = Record.Exception(() =>
+                {
+                    var sut = new TranslatableSpecification<ChildFakeType>(specification, "Message");
+                    sut.IsSatisfiedBy(new ChildFakeType(), out _);
+                });
+
+                Assert.Null(exception);
+            }
+        }
+    }
+}
---
+++ src/FluentSpecification.Core.Tests/TranslatableSpecificationTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentSpecification.Core.Tests.Mocks;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace FluentSpecification.Core.Tests
+{
+    public partial class TranslatableSpecificationTests
+    {
+        public class Constructor
+        {
+            [Fact]
+            [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
+            public void NullSpecification_ArgumentNullException()
+            {
+                var exception = Record.Exception(() =>
+                    new TranslatableSpecification<object>(null, "Custom error message"));
+
+                Assert.NotNull(exception);
+                Assert.IsType<ArgumentNullException>(exception);
+            }
+
+            [Theory]
+            [InlineData(null)]
+            [InlineData("")]
+            public void IncorrectMessage_ArgumentException(string message)
+            {
+                var exception = Record.Exception(() =>
+                    new TranslatableSpecification<object>(new MockCommonSpecification<object>(), message));
+
+                Assert.NotNull(exception);
+                Assert.IsType<ArgumentException>(exception);
+            }
+        }
+    }
+}
