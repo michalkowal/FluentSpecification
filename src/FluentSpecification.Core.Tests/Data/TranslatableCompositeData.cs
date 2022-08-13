48dc26008e6cf1eec5cbd0bcd2c60af4bfeb35f3
Michał Kowal <michalpkowal@gmail.com>
2022-08-13 16:35:05 +0200

    Add additional tests for TranslatableSpecification with CompositeSpecification (#26)

---
+++ src/FluentSpecification.Core.Tests/Data/TranslatableCompositeData.cs
@@ -0,0 +1,26 @@
+﻿using FluentSpecification.Core.Tests.Mocks;
+using FluentSpecification.Tests.Sdk.Data;
+
+namespace FluentSpecification.Core.Tests.Data
+{
+    public class TranslatableCompositeData : SpecificationData<string>
+    {
+        public TranslatableCompositeData()
+        {
+            Valid("Custom mock message")
+                .Result(2, "TrueMockComplexSpecification Mock TrueMockComplexSpecification");
+
+            Invalid("Custom mock message")
+                .Result(2, "FailedFalseMockComplexSpecification Mock FailedFalseMockComplexSpecification", c =>
+                {
+                    c.CustomError("Custom mock message");
+                    c.FailedSpecification(typeof(FalseMockComplexSpecification),
+                            "MockComplexSpecification is not satisfied")
+                        .AddParameter("Result", false);
+                    c.FailedSpecification(typeof(FalseMockComplexSpecification),
+                            "MockComplexSpecification is not satisfied")
+                        .AddParameter("Result", false);
+                });
+        }
+    }
+}
